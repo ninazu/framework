87663f15cb9c037730449b96ab2406e509e4fdbb
ninazu <ninazu@ukr.net>
2018-05-17 22:31:11 +0300

    #

--- Component/User/User.php
+++ Component/User/User.php
@@ -15,4 +15,16 @@ class User extends Component implements IUser {
 		//return self::ROLE_USER;
 		//return self::ROLE_AUTHORIZED;
 	}
+
+	private static function generateSalt() {
+		return md5(openssl_random_pseudo_bytes(16));
+	}
+
+	public static function generateToken() {
+		return hash('sha256', (openssl_random_pseudo_bytes(128)));
+	}
+
+	public static function calculateHash($password, $salt) {
+		return hash('sha256', $password . $salt);
+	}
 }
