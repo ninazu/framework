30b76111f1f9802939e2f671be49e609175028d5
ninazu <ninazu@ukr.net>
2018-07-18 21:38:10 +0300

    #

--- Form/BaseForm.php
+++ Form/BaseForm.php
@@ -4,29 +4,30 @@ namespace vendor\ninazu\framework\Form;
 
 use vendor\ninazu\framework\Component\Db\Interfaces\IConnection;
 use vendor\ninazu\framework\Component\Db\Interfaces\ITransaction;
+use vendor\ninazu\framework\Component\Response\IResponse;
 
 abstract class BaseForm {
 
+	/**@var IConnection $transaction */
 	protected $transaction;
+
+	protected $response;
 
 	protected $errors = [];
 
 	protected $attributes = [];
 
-	public function __construct(IConnection $connection) {
+	public function __construct(IResponse $response, ITransaction $connection) {
 		$this->transaction = $connection;
+		$this->response = $response;
 	}
 
 	public function load(array $requestData) {
 		//TODO
 	}
 
-	public function getResponse() {
+	public function getResult() {
 		//TODO
-		return [];
-	}
-
-	public function getMap() {
 		return [];
 	}
 
@@ -47,6 +48,10 @@ abstract class BaseForm {
 
 	public function __set($name, $value) {
 		$this->attributes[$name] = $value;
+	}
+
+	public function getAttributes() {
+		return $this->attributes;
 	}
 
 	public function addError($field, $message) {
