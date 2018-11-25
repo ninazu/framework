7e03da0a12212397e56b8844d608a0aa6d70862c
ninazu <ninazu@ukr.net>
2018-11-25 20:33:30 +0200

    #

--- Component/Telegram/TelegramBot.php
+++ Component/Telegram/TelegramBot.php
@@ -56,8 +56,6 @@ class TelegramBot extends BaseComponent {
 
 	public function updateMarkUp($chatID, $messageID, $text, $buttons, $inline = false) {
 		if (empty($messageID)) {
-			mail($this->getApplication()->getAdminEmail(), 'UpdateMarkup', $messageID);
-
 			return $this->markUp($chatID, $text, $buttons, $inline);
 		}
 
