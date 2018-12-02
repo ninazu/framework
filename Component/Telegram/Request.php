05cbcd5c3fb8b76527dbd38d57eee0d82e21152d
ninazu <ninazu@ukr.net>
2018-12-02 11:33:28 +0200

    #

--- Component/Telegram/Request.php
+++ Component/Telegram/Request.php
@@ -33,7 +33,7 @@ class Request extends BaseReader {
 			$to->isInline = false;
 			$to->isInvite = isset($content['message']['new_chat_members']);
 			$to->command = $to->isCallback ? $content['callback_query']['data'] : ($to->isInvite ? $content['message']['new_chat_members'] : (isset($content['message']['text']) ? $content['message']['text'] : null));
-			$to->isPrivateChat = ($to->isCallback ? $content['callback_query']['message']['chat']['type'] : $content['message']['chat']['type']) === 'private';
+			$to->isPrivateChat = ($to->isCallback ? $content['callback_query']['message']['chat']['type'] : isset($content['message']['chat']['type']) ? $content['message']['chat']['type'] : null) === 'private';
 			$to->messageId = $to->isCallback ? $content['callback_query']['message']['message_id'] : null;
 			$fromData = $to->isCallback ? $content['callback_query']['from'] : $content['message']['from'];
 		}
