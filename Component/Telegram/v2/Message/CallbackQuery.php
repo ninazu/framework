6945ed3b6f8bbf2c1d26a19375275aa12f37efca
ninazu <ninazu@ukr.net>
2018-12-12 21:36:29 +0200

    #

--- Component/Telegram/v2/Message/CallbackQuery.php
+++ Component/Telegram/v2/Message/CallbackQuery.php
@@ -2,14 +2,19 @@
 
 namespace vendor\ninazu\framework\Component\Telegram\v2\Message;
 
+use vendor\ninazu\framework\Component\Telegram\v2\User;
+
 /**
  * @property string $gameShortName
  * @property int $id
+ * @property User $user
  */
 class CallbackQuery extends BaseMessage {
 
 	public function setData($data) {
 		$this->id = $data['id'];
 		$this->gameShortName = $data['game_short_name'];
+		$this->user = new User();
+		$this->user->load($data['from']);
 	}
 }
