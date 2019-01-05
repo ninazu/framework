ec168d35aa52fa380c7d04418a615abd4da597eb
ninazu <ninazu@ukr.net>
2019-01-05 19:54:37 +0200

    #

---
+++ Form/Processor/SetProcessor.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace vendor\ninazu\framework\Form\Processor;
+
+use ErrorException;
+use ReflectionFunction;
+use vendor\ninazu\framework\Form\BaseProcessor;
+
+class SetProcessor extends BaseProcessor {
+
+	/**
+	 * @var \Closure $callback
+	 */
+	protected $callback;
+
+	protected $value;
+
+	public function init() {
+		if (isset($this->callback)) {
+			if (!is_callable($this->callback)) {
+				throw new ErrorException('Callback must be callable');
+			}
+
+			//$f = new ReflectionFunction($this->callback);
+			//$params = $f->getParameters();
+
+			$this->callback->bindTo($this);
+		}
+	}
+
+	public function execute(array &$data) {
+		if (isset($this->callback)) {
+			$callback = $this->callback;
+			$callback($data, $this->fields);
+		} elseif (isset($this->value)) {
+			foreach ($this->fields as $field) {
+				$data[$field] = $this->value;
+			}
+		} else {
+			throw new ErrorException('SetProcessor without callback or value in params');
+		}
+	}
+}
